a593ec928d0f15e4deaabd5bb1f178ce0fcb0bc6
YeongRoYun <58219267+YeongRoYun@users.noreply.github.com>
2023-10-10 17:06:25 +0900

    [feat] pre-commit

--- app.py
+++ app.py
@@ -1,23 +1,29 @@
 import json
 import os
 
+import dotenv
 from chalice import Chalice
 from chalice.app import SQSEvent
 
 from chalicelib.model.record import Record
 
 
+# Local debug
+if ".env" in os.listdir():
+    dotenv.load_dotenv()
+
 if os.getenv("LOG_LEVEL", "DEBUG") == "DEBUG":
     debug = True
 else:
     debug = False
 
-app = Chalice(app_name='pyoniverse-slack-alarm', debug=debug)
+app = Chalice(app_name="pyoniverse-slack-alarm", debug=debug)
 
 
-@app.on_sqs_message(queue=os.getenv("QUEUE_NAME"),
-                    batch_size=1,
-                    )
+@app.on_sqs_message(
+    queue=os.getenv("QUEUE_NAME"),
+    batch_size=1,
+)
 def send_slack(event: SQSEvent):
     for _record in event:
         record: Record = Record.load(json.loads(_record.body))
