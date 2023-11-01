c49b1e03c29ce98fcadce277e9db30202560008d
YeongRoYun <58219267+YeongRoYun@users.noreply.github.com>
2023-11-01 17:01:32 +0900

    [fix] don't propagate error

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import json
 import os
+import traceback
 from functools import reduce
 
 import dotenv
@@ -30,30 +31,34 @@ slack = Slack(log_name=app.app_name)
     batch_size=1,
 )
 def send_message(event: SQSEvent):
-    success = 0
-    total = 0
-    invalid_messages = []
-    for _record in event:
-        total += 1
-        try:
-            message: Message = Message.load(json.loads(_record.body))
-        except ValidationError as e:
-            invalid_messages.append((json.loads(_record.body), e.messages))
-        else:
-            if slack.send(message=message):
-                success += 1
-    app.log.info(f"result: {success}/{total}")
-    if invalid_messages:
-        ps = {}
-        for idx, m in enumerate(invalid_messages):
-            ps[f"reason_{idx}"] = json.dumps(m[1], ensure_ascii=False)
-            ps[f"message_{idx}"] = json.dumps(m[0], ensure_ascii=False)
-        message = Message(
-            type=MessageTypeEnum.DEBUG,
-            source=app.app_name,
-            text="Invalid Message Format",
-            ps=ps,
-            cc=reduce(lambda acc, cur: acc + cur[0]["cc"], invalid_messages, []),
-        )
-        slack.send(message)
-    return {"result": f"{success}/{total}"}
+    try:
+        success = 0
+        total = 0
+        invalid_messages = []
+        for _record in event:
+            total += 1
+            try:
+                message: Message = Message.load(json.loads(_record.body))
+            except ValidationError as e:
+                invalid_messages.append((json.loads(_record.body), e.messages))
+            else:
+                if slack.send(message=message):
+                    success += 1
+        app.log.info(f"result: {success}/{total}")
+        if invalid_messages:
+            ps = {}
+            for idx, m in enumerate(invalid_messages):
+                ps[f"reason_{idx}"] = json.dumps(m[1], ensure_ascii=False)
+                ps[f"message_{idx}"] = json.dumps(m[0], ensure_ascii=False)
+            message = Message(
+                type=MessageTypeEnum.DEBUG,
+                source=app.app_name,
+                text="Invalid Message Format",
+                ps=ps,
+                cc=reduce(lambda acc, cur: acc + cur[0]["cc"], invalid_messages, []),
+            )
+            slack.send(message)
+        return {"result": f"{success}/{total}"}
+    except Exception as e:
+        app.log.error(traceback.format_exc())
+        return None
